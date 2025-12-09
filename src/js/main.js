fc6dff2997c8d4afdc537caf48f5ccca98b1b8b1
Slava Egorov <slava.egorov@gmail.com>
2025-12-09 12:51:17 +0300

    Added header, footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import '../styles/main.scss';
+import { MobileMenu } from '../js/mobile-menu.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // Sidebar
+  const menuButton = document.querySelector('#menu-toggle');
+  const sidebarMenu = document.querySelector('#main-navigation');
+  requestAnimationFrame(() => {
+    sidebarMenu.classList.add('nav--ready');
+  });
+
+  new MobileMenu(menuButton, sidebarMenu);
+});
---
+++ src/js/mobile-menu.js
@@ -0,0 +1,44 @@
+export class MobileMenu {
+  constructor(buttonElement, sidebarElement) {
+    this.button = buttonElement;
+    this.sidebar = sidebarElement;
+
+    this.isMenuOpen = false;
+    this.scrollPosition = 0;
+
+    this.button.addEventListener('click', () => this.toggleMenu());
+  }
+
+  lockBody() {
+    this.scrollPosition = window.scrollY;
+
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${this.scrollPosition}px`;
+    document.body.style.left = '0';
+    document.body.style.right = '0';
+    document.body.style.width = '100%';
+  }
+
+  unlockBody() {
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.left = '';
+    document.body.style.right = '';
+    document.body.style.width = '';
+
+    window.scrollTo(0, this.scrollPosition);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    this.button.classList.toggle('menu-toggle--active');
+    this.sidebar.classList.toggle('nav--open');
+
+    if (this.isMenuOpen) {
+      this.lockBody();
+    } else {
+      this.unlockBody();
+    }
+  }
+}
